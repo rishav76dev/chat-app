4459a9bcc72d35d8e2ae0551ef000a90405c57a1
rishav76dev <rjrishav037@gmail.com>
2025-02-18 21:38:02 +0530

    second commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,24 @@ import express from "express";
 
 import dotenv from "dotenv";
 
-
-const app = express();
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
 
+import messageRoutes from "./routes/message.routes.js";
+
 dotenv.config();
+
+const app = express();
 
 
 app.use(express.json());
+app.use(cookiesParser());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5000,() => {
     console.log("server is running at 5001")
